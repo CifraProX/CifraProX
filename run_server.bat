0222c7fc0bff29b073d5a8c81aceb2c6b095e078
Deividy R. Oliveira <deividy.ass@gmail.com>
2026-02-03 15:22:15 -0300

    Cleanup: Remove song creation UI and legacy files

--- run_server.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Iniciando servidor Backend + Frontend (CifraProX)...
-echo O navegador nao abrira automaticamente, mas o servidor estara ouvindo.
-echo Acesse: http://localhost:3000
-echo.
-cd backend
-npm start
-pause
